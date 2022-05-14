86c4324d08ed32a037f9ba92b0c7e14b89a63f11
Peter-Anton <97073590+Peter-Anton@users.noreply.github.com>
2022-05-14 17:06:50 +0200

    dddd

---
+++ cmake-build-debug/CMakeFiles/dictionary.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dictionary.dir/BTree.c.obj"
+  "CMakeFiles/dictionary.dir/BTree.c.obj.d"
+  "CMakeFiles/dictionary.dir/main.c.obj"
+  "CMakeFiles/dictionary.dir/main.c.obj.d"
+  "dictionary.exe"
+  "dictionary.exe.manifest"
+  "dictionary.pdb"
+  "libdictionary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
